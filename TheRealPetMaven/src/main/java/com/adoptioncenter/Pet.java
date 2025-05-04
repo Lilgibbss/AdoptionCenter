2c2a97ac110a5f41a6e66e8ee404f6b604a80cc4
Lilgibbss <134891354+Lilgibbss@users.noreply.github.com>
2025-05-04 16:12:17 -0500

    Getters setters and constructors

--- TheRealPetMaven/src/main/java/com/adoptioncenter/Pet.java
+++ TheRealPetMaven/src/main/java/com/adoptioncenter/Pet.java
@@ -1,9 +1,79 @@
 package com.adoptioncenter;
 
 public class Pet {
+	private String ID;
     private String name;
-    private int age;
     private String species;
+    private String subspecies;
+    private int age;
+    private Boolean adopted;
+    
 
     // Constructors, Getters, Setters
+    // Default constructor (for Gson)
+    public Pet() {}
+
+    // Full constructor
+    public Pet(String ID, String name, String species, String subspecies, int age, Boolean adopted) {
+        this.ID = ID;
+        this.name = name;
+        this.species = species;
+        this.subspecies = subspecies;
+        this.age = age;
+        this.adopted = adopted;
+    }
+
+    // Getters and Setters
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getSubspecies() {
+        return subspecies;
+    }
+
+    public void setSubspecies(String subspecies) {
+        this.subspecies = subspecies;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Boolean getAdopted() {
+        return adopted;
+    }
+
+    public void setAdopted(Boolean adopted) {
+        this.adopted = adopted;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + species + ", " + age + " yrs)";
+    }
 }
