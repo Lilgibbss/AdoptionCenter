ff7b1f759389d0d6bc6da58da3cd6c88770aeda2
suneetghantaji <suneetghantaji@mac.lan>
2025-05-04 20:22:43 -0500

    I worked on the code for Rabbit,Cat,Dog and integrated them with Pet

---
+++ TheRealPetMaven/src/main/java/com/adoptioncenter/Cat.java
@@ -0,0 +1,8 @@
+package com.adoptioncenter;
+
+public class Cat extends Pet {
+	public Cat(String name, int age) {
+		super(name, age, "Cat");
+	}
+
+}
---
+++ TheRealPetMaven/src/main/java/com/adoptioncenter/Rabbit.java
@@ -0,0 +1,8 @@
+package com.adoptioncenter;
+
+public class Rabbit extends Pet {
+	public Rabbit(String name, int age) {
+		super(name, age, "Rabbit");
+	}
+
+}
